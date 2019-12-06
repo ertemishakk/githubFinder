67f3cea5f4530588cd1d7d4efd8f2b6e954fe050
Ishak Ertem <ertemishakk@gmail.com>
2019-12-06 12:07:19 +1100

    Updates

---
+++ precache-manifest.de268db2df4dc6d8e29a3b11b23a699f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3674a0780c9782cd91a5cb2fd3f2d4",
+    "url": "/githubFinder/index.html"
+  },
+  {
+    "revision": "af8d8a3c17f2354cb93b",
+    "url": "/githubFinder/static/js/2.a4b52d86.chunk.js"
+  },
+  {
+    "revision": "9cae0f466a555b547b79",
+    "url": "/githubFinder/static/js/main.7118525f.chunk.js"
+  },
+  {
+    "revision": "12ac1f4fe2a158f29b7c",
+    "url": "/githubFinder/static/js/runtime-main.ae70c167.js"
+  },
+  {
+    "revision": "3f7787e33802c7dac0bebbc1dd7117ae",
+    "url": "/githubFinder/static/media/spinner.3f7787e3.gif"
+  }
+]);
